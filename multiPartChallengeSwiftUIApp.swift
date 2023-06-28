f0b765791b65d11082fbaf84bbb9d225e12191e1
ethanBalink <136306997+ethanBalink@users.noreply.github.com>
2023-06-28 12:02:06 +0300

    Initial commit

---
+++ multiPartChallengeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiPartChallengeSwiftUIApp.swift
+//  multiPartChallengeSwiftUI
+//
+//  Created by EthanBalink on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct multiPartChallengeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
